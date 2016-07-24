65315426572fda7f2c73bdc483da5106dd329694
jl5211 <jl5211@nyu.edu>
2016-07-24 14:37:10 -0400

    Passcode from Niela
    This is the first update to the github from niela’s laptop

---
+++ Pass/FriendsMainViewController.swift
@@ -0,0 +1,40 @@
+//
+//  FriendsMainViewController.swift
+//  Pass
+//
+//  Created by Niela Sultana on 7/17/16.
+//  Copyright © 2016 Jesus Leal. All rights reserved.
+//
+
+import UIKit
+
+class FriendsMainViewController:
+
+
+    UIViewController {
+
+    @IBOutlet weak var friendsTableView: UITableView!
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pass
+//
+//  Created by Jesus Leal on 7/9/16.
+//  Copyright © 2016 Jesus Leal. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
